56407c2098111515e93f2bbd276630d1e31840f5
joshoxe <joshoxenham1@gmail.com>
2020-10-18 17:48:28 +0100

    Add letter cell to loop value method

--- src/js/cells.js
+++ src/js/cells.js
@@ -11,6 +11,18 @@ const letteredCells = {
     "8": "I",
 }
 
+const loopValues = {
+    "A": 0,
+    "B": 1,
+    "C": 2,
+    "D": 3,
+    "E": 4,
+    "F": 5,
+    "G": 6,
+    "H": 7,
+    "I": 8,
+}
+
 /** Mappings for cells from backend and frontend */
 class cells {
 
@@ -23,6 +35,17 @@ class cells {
         i = i.toString();
         // Add 1 to j match loop value (starting from 0) to real value
         j = (j+1).toString();
-        return `${letteredCells[i]}${j}`
+        return `${letteredCells[i]}${j}`;
+    }
+
+    /***
+     *  Get an i, j loop value pair from a lettered cell
+     * @param {string} letteredCell A1-I9
+     */
+    static getLoopValue(letteredCell) {
+        let i = loopValues[letteredCell.charAt(0)];
+        let j = letteredCell.charAt(1);
+
+        return [i, j - 1]
     }
 }
